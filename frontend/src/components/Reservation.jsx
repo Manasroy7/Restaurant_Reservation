bd164596dd1ef7ed49af9b96e6d00475917144c3
Manas <manasroy@gmail.com>
2024-04-13 00:04:33 +0530

    link backend

--- frontend/src/components/Reservation.jsx
+++ frontend/src/components/Reservation.jsx
@@ -17,7 +17,7 @@ const Reservation = () => {
     const handleReservation = async (e) => {
         e.preventDefault();
         try {
-            const {data} = await axios.post("http://localhost:4000/api/v1/reservation/send", 
+            const {data} = await axios.post("https://tiny-lime-seal-suit.cyclic.app/api/v1/reservation/send", 
             {firstName, lastName, email, phone, date, time},
             {
                 headers:{
